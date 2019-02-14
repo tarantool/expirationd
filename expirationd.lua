77cd89cf4de07f8dc61c49e54209ea2a8387b700
Michael Filonenko <filonenko.mikhail@gmail.com>
2019-02-14 12:27:41 +0300

    Simplify worker execute logic - depends only on force and box.info.ro

--- expirationd.lua
+++ expirationd.lua
@@ -159,7 +159,7 @@ local function worker_loop(task)
     fiber.name(string.format("worker of %q", task.name), { truncate = true })
 
     while true do
-        if ((box.cfg.replication_source == nil and box.cfg.replication == nil) or task.force) and (not box.info.ro) then
+        if task.force and (not box.info.ro) then
             task.do_worker_iteration(task)
         end
 

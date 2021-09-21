09dd5a03739581c3d712b69f49e49dc437db3e3a
Sergey Bronnikov <sergeyb@tarantool.org>
2021-09-21 21:11:16 +0300

    test: avoid using loops for parameterization
    Part of #80

--- test/unit/iterator_type_test.lua
+++ test/unit/iterator_type_test.lua
@@ -126,43 +126,49 @@ function g.test_passing_le()
     task:kill()
 end
 
+local function test_tree_index_all(space)
+    -- without start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
+
+    local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "ALL"})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {1, "3"},
+            {2, "2"},
+            {3, "1"}
+        })
+    end)
+    task:kill()
+
+    -- with start_key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
+
+    task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "ALL", start_key = 2})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {2, "2"},
+            {3, "1"}
+        })
+    end)
+    task:kill()
+end
+
+function g.test_tree_index_all_vinyl()
+    test_tree_index_all(g.vinyl)
+end
+
 function g.test_tree_index_all()
-    for _, space in pairs({g.tree, g.vinyl}) do
-        -- without start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
-
-        local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "ALL"})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {1, "3"},
-                {2, "2"},
-                {3, "1"}
-            })
-        end)
-        task:kill()
-
-        -- with start_key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
-
-        task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "ALL", start_key = 2})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {2, "2"},
-                {3, "1"}
-            })
-        end)
-        task:kill()
-    end
+    test_tree_index_all(g.tree)
 end
 
 function g.test_hash_index_all()
@@ -204,44 +210,50 @@ function g.test_hash_index_all()
     task:kill()
 end
 
+local function test_tree_index_eq(space)
+    -- without start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
+
+    local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "EQ"})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {1, "3"},
+            {2, "2"},
+            {3, "1"}
+        })
+    end)
+    task:kill()
+
+    -- with start_key
+    helpers.iteration_result = {}
+    space:insert({1, "1", nil, nil, 1})
+    space:insert({2, "2", nil, nil, 2})
+    space:insert({3, "3", nil, nil, 2})
+    space:insert({4, "4", nil, nil, 4})
+
+    task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "EQ", index = "non_unique_index", start_key = 2})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {2, "2", nil, nil, 2},
+            {3, "3", nil, nil, 2}
+        })
+    end)
+    task:kill()
+end
+
+function g.test_tree_index_eq_vinyl()
+    test_tree_index_eq(g.vinyl)
+end
+
 function g.test_tree_index_eq()
-    for _, space in pairs({g.tree, g.vinyl}) do
-        -- without start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
-
-        local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "EQ"})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {1, "3"},
-                {2, "2"},
-                {3, "1"}
-            })
-        end)
-        task:kill()
-
-        -- with start_key
-        helpers.iteration_result = {}
-        space:insert({1, "1", nil, nil, 1})
-        space:insert({2, "2", nil, nil, 2})
-        space:insert({3, "3", nil, nil, 2})
-        space:insert({4, "4", nil, nil, 4})
-
-        task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "EQ", index = "non_unique_index", start_key = 2})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {2, "2", nil, nil, 2},
-                {3, "3", nil, nil, 2}
-            })
-        end)
-        task:kill()
-    end
+    test_tree_index_eq(g.tree)
 end
 
 function g.test_hash_index_eq()
@@ -270,42 +282,48 @@ function g.test_hash_index_eq()
     task:kill()
 end
 
+local function test_tree_index_gt(space)
+    -- without start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
+
+    local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "GT"})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {1, "3"},
+            {2, "2"},
+            {3, "1"}
+        })
+    end)
+    task:kill()
+
+    -- with start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
+
+    task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "GT", start_key = 2})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {3, "1"}
+        })
+    end)
+    task:kill()
+end
+
+function g.test_tree_index_gt_vinyl()
+    test_tree_index_gt(g.vinyl)
+end
+
 function g.test_tree_index_gt()
-    for _, space in pairs({g.tree, g.vinyl}) do
-        -- without start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
-
-        local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "GT"})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {1, "3"},
-                {2, "2"},
-                {3, "1"}
-            })
-        end)
-        task:kill()
-
-        -- with start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
-
-        task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "GT", start_key = 2})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {3, "1"}
-            })
-        end)
-        task:kill()
-    end
+    test_tree_index_gt(g.tree)
 end
 
 function g.test_hash_index_gt()
@@ -346,158 +364,182 @@ function g.test_hash_index_gt()
     task:kill()
 end
 
+local function test_tree_index_req(space)
+    -- without start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
+
+    local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "REQ"})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {3, "1"},
+            {2, "2"},
+            {1, "3"}
+        })
+    end)
+    task:kill()
+
+    -- with start_key
+    helpers.iteration_result = {}
+    space:insert({1, "1", nil, nil, 1})
+    space:insert({2, "2", nil, nil, 2})
+    space:insert({3, "3", nil, nil, 2})
+    space:insert({4, "4", nil, nil, 4})
+
+    task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "REQ", index = "non_unique_index", start_key = 2})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {3, "3", nil, nil, 2},
+            {2, "2", nil, nil, 2}
+        })
+    end)
+    task:kill()
+end
+
+function g.test_tree_index_req_vinyl()
+    test_tree_index_req(g.vinyl)
+end
+
 function g.test_tree_index_req()
-    for _, space in pairs({g.tree, g.vinyl}) do
-        -- without start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
+    test_tree_index_req(g.tree)
+end
 
-        local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "REQ"})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {3, "1"},
-                {2, "2"},
-                {1, "3"}
-            })
-        end)
-        task:kill()
+local function test_tree_index_ge(space)
+    -- without start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
 
-        -- with start_key
-        helpers.iteration_result = {}
-        space:insert({1, "1", nil, nil, 1})
-        space:insert({2, "2", nil, nil, 2})
-        space:insert({3, "3", nil, nil, 2})
-        space:insert({4, "4", nil, nil, 4})
+    local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "GE"})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {1, "3"},
+            {2, "2"},
+            {3, "1"}
+        })
+    end)
+    task:kill()
 
-        task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "REQ", index = "non_unique_index", start_key = 2})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {3, "3", nil, nil, 2},
-                {2, "2", nil, nil, 2}
-            })
-        end)
-        task:kill()
-    end
+    -- with start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
+
+    task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "GE", start_key = 2})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {2, "2"},
+            {3, "1"}
+        })
+    end)
+    task:kill()
 end
 
 function g.test_tree_index_ge()
-    for _, space in pairs({g.tree, g.vinyl}) do
-        -- without start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
+    test_tree_index_ge(g.vinyl)
+end
 
-        local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "GE"})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {1, "3"},
-                {2, "2"},
-                {3, "1"}
-            })
-        end)
-        task:kill()
+function g.test_tree_index_ge()
+    test_tree_index_ge(g.tree)
+end
 
-        -- with start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
+local function test_tree_index_lt(space)
+    -- without start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
 
-        task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "GE", start_key = 2})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {2, "2"},
-                {3, "1"}
-            })
-        end)
-        task:kill()
-    end
+    local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "LT"})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {3, "1"},
+            {2, "2"},
+            {1, "3"}
+        })
+    end)
+    task:kill()
+
+    -- with start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
+
+    task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "LT", start_key = 2})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {1, "3"}
+        })
+    end)
+    task:kill()
+end
+
+function g.test_tree_index_lt_vinyl()
+    test_tree_index_lt(g.vinyl)
 end
 
 function g.test_tree_index_lt()
-    for _, space in pairs({g.tree, g.vinyl}) do
-        -- without start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
+    test_tree_index_lt(g.tree)
+end
 
-        local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "LT"})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {3, "1"},
-                {2, "2"},
-                {1, "3"}
-            })
-        end)
-        task:kill()
+local function test_tree_index_le(space)
+    -- without start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
 
-        -- with start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
+    local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "LE"})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {3, "1"},
+            {2, "2"},
+            {1, "3"}
+        })
+    end)
+    task:kill()
 
-        task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "LT", start_key = 2})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {1, "3"}
-            })
-        end)
-        task:kill()
-    end
+    -- with start key
+    helpers.iteration_result = {}
+    space:insert({1, "3"})
+    space:insert({2, "2"})
+    space:insert({3, "1"})
+
+    task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
+            {iterator_type = "LE", start_key = 2})
+    -- wait for tuples expired
+    helpers.retrying({}, function()
+        t.assert_equals(helpers.iteration_result, {
+            {2, "2"},
+            {1, "3"}
+        })
+    end)
+    task:kill()
+end
+
+function g.test_tree_index_le_vinyl()
+    test_tree_index_le(g.vinyl)
 end
 
 function g.test_tree_index_le()
-    for _, space in pairs({g.tree, g.vinyl}) do
-        -- without start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
-
-        local task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "LE"})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {3, "1"},
-                {2, "2"},
-                {1, "3"}
-            })
-        end)
-        task:kill()
-
-        -- with start key
-        helpers.iteration_result = {}
-        space:insert({1, "3"})
-        space:insert({2, "2"})
-        space:insert({3, "1"})
-
-        task = expirationd.start("clean_all", space.id, helpers.is_expired_debug,
-                {iterator_type = "LE", start_key = 2})
-        -- wait for tuples expired
-        helpers.retrying({}, function()
-            t.assert_equals(helpers.iteration_result, {
-                {2, "2"},
-                {1, "3"}
-            })
-        end)
-        task:kill()
-    end
+    test_tree_index_le(g.tree)
 end

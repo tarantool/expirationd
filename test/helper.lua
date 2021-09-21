87f7f4d3c304898fec84de13bc3980c56f56222f
Sergey Bronnikov <sergeyb@tarantool.org>
2021-09-21 18:42:29 +0300

    test: use common space format in setup
    Part of #80

--- test/helper.lua
+++ test/helper.lua
@@ -12,8 +12,7 @@ t.before_suite(function()
         vinyl_memory = 1024,
     }
 
-    local tree = box.schema.create_space("tree", { if_not_exists = true })
-    tree:format({
+    local space_format = {
         {name = "id", type = "number"},
         {name = "first_name", type = "string"},
         {name = "value", type = "number", is_nullable = true},
@@ -22,33 +21,23 @@ t.before_suite(function()
         {name = "json_path_field", is_nullable = true, unique = false},
         {name = "multikey_field", is_nullable = true},
         {name = "functional_field", is_nullable = true},
-    })
+    }
+
+    local tree = box.schema.create_space("tree", { if_not_exists = true })
+    tree:format(space_format)
     tree:create_index("primary", {type = "TREE", parts={ 1 }})
     tree:create_index("index_for_first_name", {type = "TREE", parts={ 2 }})
     tree:create_index("multipart_index", {type = "TREE", parts={ {3, is_nullable = true}, {4, is_nullable = true} }})
     tree:create_index("non_unique_index", {type = "TREE", parts={ {5, is_nullable = true} }, unique = false})
 
     local hash = box.schema.create_space("hash", { if_not_exists = true })
-    hash:format({
-        {name = "id", type = "number"},
-        {name = "first_name", type = "string"},
-        {name = "value", type = "number", is_nullable = true},
-        {name = "count", type = "number", is_nullable = true}
-    })
+    hash:format(space_format)
     hash:create_index("primary", {type = "HASH", parts={ 1 }} )
     hash:create_index("index_for_first_name", {type = "HASH", parts={ 2 }} )
     hash:create_index("multipart_index", {type = "HASH", parts={ {1}, {2} }})
 
     local vinyl = box.schema.create_space("vinyl", { if_not_exists = true, engine = "vinyl" })
-    vinyl:format({
-        {name = "id", type = "number"},
-        {name = "first_name", type = "string"},
-        {name = "value", type = "number", is_nullable = true},
-        {name = "count", type = "number", is_nullable = true},
-        {name = "non_unique_id", type = "number", is_nullable = true, unique = false},
-        {name = "json_path_field", is_nullable = true, unique = false},
-        {name = "multikey_field", is_nullable = true, unique = false},
-    })
+    vinyl:format(space_format)
     vinyl:create_index("primary", {type = "TREE", parts={ 1 }})
     vinyl:create_index("index_for_first_name", {type = "TREE", parts={ 2 }})
     vinyl:create_index("multipart_index", {type = "TREE", parts={ {3, is_nullable = true}, {4, is_nullable = true} }})
@@ -77,6 +66,7 @@ t.before_suite(function()
     end
 
     local bitset = box.schema.create_space("bitset", { if_not_exists = true })
+    bitset:format(space_format)
     bitset:create_index("primary", {type = "TREE", parts={ 1 }})
     bitset:create_index("index_for_first_name",
             {type = "BITSET", parts={ {field = 2, type = "string"} }, unique = false})
